aca90a46011639bbd07da4ddfff755259c1973bb
Thanuja <156277448+ThanujaPinninti@users.noreply.github.com>
2025-09-20 06:06:19 +0530

    Practice

---
+++ Programs/FirstProgram.java
@@ -0,0 +1,13 @@
+/*
+ * This is a simple Java Program
+ * Call this file "FirstProgram.java"
+ */
+public class FirstProgram {
+    // Your program begins with call to main()
+    public static void main(String[] args) {
+        System.out.println("Ths is a simple Java program");
+    } 
+}
+/*javac FirstProgram.java
+java FirstProgram      
+Ths is a simple Java program */
---
+++ Programs/SecondProgram.java
@@ -0,0 +1,20 @@
+/*Here is another short example
+ * Call this file file SecondProgram.java
+ */
+public class SecondProgram {
+    public static void main(String[] args) {
+        int num;
+        num=100;
+        System.out.println("This is num:"+num);
+        num = num*2;
+        System.out.println("Th value of num * 2 is ");
+        System.out.println(num);
+    }
+}
+/*
+ * javac SecondProgram.java
+java SecondProgram      
+This is num:100
+Th value of num * 2 is 
+200
+ */
